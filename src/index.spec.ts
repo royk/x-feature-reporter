897bb7c8e76421134b2e1e4646ccc9fe8a4ebdcc
Roy Klein <roeiklein@gmail.com>
2024-10-26 12:29:10 +0200

    test: add test case

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -28,79 +28,122 @@ test.afterEach(() => {
 });
 
 test.describe("Features", () => {
-  test("Suites appear as headings. Nested Suites are nested headings", () => {
-    const testSuite: TestSuite = {
-      title: featureTitle,
-      suites: [],
-      tests: []
-    };
-    const testSuite2: TestSuite = {
-      title: subfeatureTitle,
-      suites: [],
-      tests: []
-    };
-    testSuite.suites.push(testSuite2);
-    const testCase: TestResult = {
-      title: caseTitle,
-      status: 'passed',
-      testType: TEST_TYPE_BEHAVIOR
-    };
-    testSuite2.tests.push(testCase);
-    reporter.generateReport(outputFile, testSuite);
+  test.describe("Suites", () => {
+    test("Suites appear as headings. Nested Suites are nested headings", () => {
+      const testSuite: TestSuite = {
+        title: featureTitle,
+        suites: [],
+        tests: []
+      };
+      const testSuite2: TestSuite = {
+        title: subfeatureTitle,
+        suites: [],
+        tests: []
+      };
+      testSuite.suites.push(testSuite2);
+      const testCase: TestResult = {
+        title: caseTitle,
+        status: 'passed',
+        testType: TEST_TYPE_BEHAVIOR
+      };
+      testSuite2.tests.push(testCase);
+      reporter.generateReport(outputFile, testSuite);
 
-    const expectedMarkdown = `\n## ${featureTitle}\n  ### ${subfeatureTitle}\n  - ${passingEmoji} ${caseTitle}\n`;
-    const actualMarkdown = writeFileSyncStub.getCall(0)?.args[1];
-    
-    expect(actualMarkdown).toBe(expectedMarkdown);
+      const expectedMarkdown = `\n## ${featureTitle}\n  ### ${subfeatureTitle}\n  - ${passingEmoji} ${caseTitle}\n`;
+      const actualMarkdown = writeFileSyncStub.getCall(0)?.args[1];
+      
+      expect(actualMarkdown).toBe(expectedMarkdown);
+    });
+    test("Suites can be marked as transparent: They will not be printed but their children will be printed", () => {
+      const rootSuite: TestSuite = {
+        title: 'dont print',
+        transparent: true,
+        suites: [],
+        tests: []
+      };
+  
+      const testSuite: TestSuite = {
+        title: featureTitle,
+        suites: [],
+        tests: []
+      };
+      rootSuite.suites.push(testSuite);
+      const testCase: TestResult = {
+        title: caseTitle,
+        status: 'passed',
+        testType: TEST_TYPE_BEHAVIOR
+      };
+      testSuite.tests.push(testCase);
+      reporter.generateReport(outputFile, rootSuite);
+  
+      const expectedMarkdown = `\n## ${featureTitle}\n- ${passingEmoji} ${caseTitle}\n`;
+      const actualMarkdown = writeFileSyncStub.getCall(0)?.args[1];
+      
+      expect(actualMarkdown).toBe(expectedMarkdown);
+    });
+    test("Suites with the same name have their results merged, and the heading is shown only once", () => {
+      const rootSuite: TestSuite = {
+        title: 'dont print',
+        transparent: true,
+        suites: [],
+        tests: []
+      };
+  
+      const testSuite1: TestSuite = {
+        title: featureTitle,
+        suites: [],
+        tests: []
+      };
+      const testSuite2: TestSuite = {
+        title: featureTitle,
+        suites: [],
+        tests: []
+      };
+      rootSuite.suites.push(testSuite1);
+      rootSuite.suites.push(testSuite2);
+      const testCase1: TestResult = {
+        title: caseTitle,
+        status: 'passed',
+        testType: TEST_TYPE_BEHAVIOR
+      };
+      testSuite1.tests.push(testCase1);
+      const testCase2: TestResult = {
+        title: caseTitle2,
+        status: 'passed',
+        testType: TEST_TYPE_BEHAVIOR
+      };
+      testSuite2.tests.push(testCase2);
+      reporter.generateReport(outputFile, rootSuite);
+
+      const expectedMarkdown = `\n## ${featureTitle}\n- ${passingEmoji} ${caseTitle}\n- ${passingEmoji} ${caseTitle2}\n`;
+      const actualMarkdown = writeFileSyncStub.getCall(0)?.args[1];
+      expect(actualMarkdown).toBe(expectedMarkdown);
+    });
   });
+  test.describe("TestResults", () => {
   test(`TestResults appear as list items representing features. Each feature is visually marked as Passing ${passingEmoji}, Failing ${failingEmoji} or Skipped ${skippedEmoji}`, () => {
     const testSuite: TestSuite = {
       title: featureTitle,
       suites: [],
       tests: []
     };
-    const failedCase: TestResult = {
-      title: caseTitle,
-      status: 'failed',
-    };
-    const skippedCase: TestResult = {
-      title: caseTitle2,
-      status: 'skipped',
-    };
-    testSuite.tests.push(failedCase);
-    testSuite.tests.push(skippedCase);
-    reporter.generateReport(outputFile, testSuite);
-    const expectedMarkdown = `\n## ${featureTitle}\n- ${failingEmoji} ${caseTitle}\n- ${skippedEmoji} ${caseTitle2}\n`;
-    const actualMarkdown = writeFileSyncStub.getCall(0)?.args[1];
-    expect(actualMarkdown).toBe(expectedMarkdown);
+      const failedCase: TestResult = {
+        title: caseTitle,
+        status: 'failed',
+      };
+      const skippedCase: TestResult = {
+        title: caseTitle2,
+        status: 'skipped',
+      };
+      testSuite.tests.push(failedCase);
+      testSuite.tests.push(skippedCase);
+      reporter.generateReport(outputFile, testSuite);
+      const expectedMarkdown = `\n## ${featureTitle}\n- ${failingEmoji} ${caseTitle}\n- ${skippedEmoji} ${caseTitle2}\n`;
+      const actualMarkdown = writeFileSyncStub.getCall(0)?.args[1];
+      expect(actualMarkdown).toBe(expectedMarkdown);
+    });
   });
-  test("Suites can be marked as transparent: They will not be printed but their children will be printed", () => {
-    const rootSuite: TestSuite = {
-      title: 'dont print',
-      transparent: true,
-      suites: [],
-      tests: []
-    };
-
-    const testSuite: TestSuite = {
-      title: featureTitle,
-      suites: [],
-      tests: []
-    };
-    rootSuite.suites.push(testSuite);
-    const testCase: TestResult = {
-      title: caseTitle,
-      status: 'passed',
-      testType: TEST_TYPE_BEHAVIOR
-    };
-    testSuite.tests.push(testCase);
-    reporter.generateReport(outputFile, rootSuite);
-
-    const expectedMarkdown = `\n## ${featureTitle}\n- ${passingEmoji} ${caseTitle}\n`;
-    const actualMarkdown = writeFileSyncStub.getCall(0)?.args[1];
-    
-    expect(actualMarkdown).toBe(expectedMarkdown);
-  });
+  
   test("A link to a full test report will be included when the 'fullReportLink' option is provided", () => {
     const fullReportLink = 'full-report.html';
     const testSuite: TestSuite = {
